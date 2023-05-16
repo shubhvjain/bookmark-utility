8fe9e476f09be558b2ef369db92a8422fc53211f
shubh <shubhvjain@gmail.com>
2023-05-16 07:56:15 +0200

    bookmarklet generator on index

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
-const NewNotePageObsidian = () => {
-  let data = {
-    title: document.title,
-    accessedOn: new Date().toLocaleString(),
-    fileName: "",
-    url: window.location.href,
-  };
-  console.log(data);
+const getPageData = ()=>{
+  let data = { title: document.title, accessedOn: new Date().toLocaleString(), fileName: "", url: window.location.href};
   data.fileName = data.title.replace(/[^A-Z0-9]/gi, "-").toLowerCase();
+  return data
+}
+
+const NewNotePageObsidian = () => {
+  let data = getPageData()
   let blockObject = `${data.fileName}
 
 .[source, object]
@@ -14,10 +13,20 @@ const NewNotePageObsidian = () => {
 - accessed : ${data.accessedOn}
 - title : ${data.title}
 
+`;
+  navigator.clipboard.writeText(blockObject);
+};
+
+const NewReadingListItem = () => {
+  let data = getPageData()
+  let blockObject = `.[${data.fileName}, object]
+- url : ${data.url}
+- accessed : ${data.accessedOn}
+- title : ${data.title}
+- read : false
 
 `;
   navigator.clipboard.writeText(blockObject);
-  console.log("Text copied to clipboard");
 };
 
 const buttons = [
@@ -25,6 +34,11 @@ const buttons = [
     title: "+ New page",
     note: "To generate a new page in Obsidian to take website notes",
     method: NewNotePageObsidian,
+  },
+ {
+    title: "+ New Reading list item",
+    note: "To generate a item to add to a reading list",
+    method: NewReadingListItem,
   },
 ];
 
@@ -42,7 +56,7 @@ const loadHTMLModel = () => {
 	// https://www.codexworld.com/simple-modal-popup-javascript-css/
   let html = `<style>.mpopup{display:none;position:fixed;z-index:1;padding-top:100px;left:0;top:0;width:100%;height:100%;overflow:auto;background-color:rgba(0,0,0,.4)}.modal-content,.modal-footer,.modal-header{background-color:#fff}.modal-content{position:relative;margin:auto;padding:0;width:450px;box-shadow:0 4px 8px 0 rgba(0,0,0,.2),0 6px 20px 0 rgba(0,0,0,.19);-webkit-animation-name:animatetop;-webkit-animation-duration:.4s;animation-name:animatetop;animation-duration:.4s;border-radius:.3rem}.modal-body,.modal-header{padding:2px 12px}.modal-header{color:#333;border-bottom:1px solid #e9ecef;border-top-left-radius:.3rem;border-top-right-radius:.3rem}.modal-header h2{font-size:1.25rem;margin-top:14px;margin-bottom:14px}.modal-footer{padding:1rem;color:#333;border-top:1px solid #e9ecef;border-bottom-left-radius:.3rem;border-bottom-right-radius:.3rem;text-align:right}.close{color:#888;float:right;font-size:28px;font-weight:700}.close:focus,.close:hover{color:#000;text-decoration:none;cursor:pointer}@-webkit-keyframes animatetop{from{top:-300px;opacity:0}to{top:0;opacity:1}}@keyframes animatetop{from{top:-300px;opacity:0}to{top:0;opacity:1}}</style>
 	<div id="mpopupBox" class="mpopup"><div class="modal-content">
-  <div class="modal-header"><span class="close">×</span><h2>Automation tasks</h2></div>
+  <div class="modal-header"><span class="close">x</span><h2>Automation tasks</h2></div>
   <div class="modal-body">${btncontent}</div>
   <div class="modal-footer1"></div></div></div>`;
   const modalDiv = document.createElement("div");
@@ -82,4 +96,4 @@ const loadModal = () => {
 // window.onload = async function () {
 //   loadModal();
 // };
-// loadModal();
+loadModal();
